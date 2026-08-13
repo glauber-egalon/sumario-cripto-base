f0c3a8d57fd2e45ff61924f5d97728681e850a16
glauber-egalon <102186643+glauber-egalon@users.noreply.github.com>
2026-08-13 16:23:46 -0300

    Add withdrawal support to SumarioCriptoCaixinha

--- contracts/SumarioCriptoCaixinha.sol
+++ contracts/SumarioCriptoCaixinha.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.20;
 
 contract SumarioCriptoCaixinha {
 
+    address public dono;
+
     struct Pagamento {
         address pagador;
         uint256 valor;
@@ -10,6 +12,10 @@ contract SumarioCriptoCaixinha {
     }
 
     Pagamento[] public pagamentos;
+
+    constructor() {
+        dono = msg.sender;
+    }
 
     function depositar() public payable {
         require(msg.value > 0, "Envie algum ETH");
@@ -21,6 +27,14 @@ contract SumarioCriptoCaixinha {
                 data: block.timestamp
             })
         );
+    }
+
+    function sacar(uint256 valor) public {
+        require(msg.sender == dono, "Apenas o dono");
+        require(valor <= address(this).balance, "Saldo insuficiente");
+
+        (bool sucesso, ) = payable(dono).call{value: valor}("");
+        require(sucesso, "Falha ao sacar");
     }
 
     function quantidadePagamentos() public view returns (uint256) {
